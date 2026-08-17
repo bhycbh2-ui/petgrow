096f147f702b1f2a11903da72c1707d3c1807531
bhycbh2-ui <bhycbh2@gmail.com>
2026-08-17 22:04:54 +0900

    style: load premium responsive design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./petgrow-premium-20260817.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
