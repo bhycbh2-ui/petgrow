85e9ac10336556b3a971dfcf1fca9930b9fa16bb
bhycbh2-ui <bhycbh2@gmail.com>
2026-08-18 10:38:19 +0900

    Restore PetPoint client and refresh service worker

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./petgrow-premium-20260817.css";
 import "./final-ux-20260818.css";
 import "./ui-fixes-20260818.css";
-import "./point-removal-20260818.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -15,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 // PWA: 서비스워커 등록 (오프라인 캐싱 + 홈화면 추가 지원)
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js?v=27", { updateViaCache: "none" })
+    navigator.serviceWorker.register("/sw.js?v=28", { updateViaCache: "none" })
       .then((registration) => registration.update())
       .catch(() => {});
   });
